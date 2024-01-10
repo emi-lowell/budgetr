e4948034f1c8bcec9b64483be64d6be4be7725a9
@emi-lowell <edlowell@gmail.com>
2024-01-10 11:48:57 -0500

    fixed license and chart, added reactable

--- R/mod_dashboard.R
+++ R/mod_dashboard.R
@@ -7,6 +7,7 @@
 #' @noRd
 #'
 #' @import shinydashboard
+#' @import reactable
 #' @import dplyr
 #' @import lubridate
 #' @import scales
@@ -29,7 +30,8 @@ mod_dashboard_ui <- function(id) {
     valueBoxOutput(outputId = ns("year_spend_value")),
     valueBoxOutput(outputId = ns("year_income_value")),
     valueBoxOutput(outputId = ns("year_diff_value")),
-    girafeOutput(outputId = ns("year_chart"))
+    column(6, girafeOutput(outputId = ns("year_chart"))),
+    column(6, reactableOutput(outputId = ns("year_breakdown")))
   )
 }
 
@@ -73,29 +75,42 @@ mod_dashboard_server <- function(id) {
         ) %>%
         mutate(type = "Expense")
 
-      year_data <- bind_rows(income_df_prep, expense_df_prep) %>%
+      bind_exp_inc <- bind_rows(income_df_prep, expense_df_prep)
+
+      year_data <- bind_exp_inc %>%
         mutate(date_year = year(date)) %>%
+        filter(date_year == yearzzz()) %>%
         group_by(date_year, category, type) %>%
         summarise(year_amount = sum(amount, na.rm = TRUE), .groups = "drop")
 
+
+
       chartzzz <- year_data %>%
         ggplot() +
-        geom_col_interactive(aes(
-          x = as.character(date_year),
-          y = year_amount,
-          fill = type)
+        geom_col_interactive(
+          aes(
+            x = as.character(date_year),
+            y = year_amount,
+            fill = type
+          ),
+          width = .1
         ) +
         theme_minimal() +
         scale_fill_manual(values = c(
           "#990000",
           "#009900"
         )) +
-        scale_y_continuous(labels = scales::dollar_format()) +
+        scale_y_continuous(
+          labels = scales::dollar_format(),
+          limits = c(0, 150000)
+        ) +
         labs(
           x = "Year",
           y = "Value"
         ) +
-        guides(fill = guide_legend_interactive(title = "Type"))
+        guides(fill = guide_legend_interactive(title = "Type")) +
+        theme(panel.background = element_rect(color = "#00000000"),
+              )
 
       output$year_spend_value <- renderValueBox({
         valueBox(
@@ -126,7 +141,37 @@ mod_dashboard_server <- function(id) {
 
 
       output$year_chart <- renderGirafe({
-        girafe(ggobj = chartzzz)
+        girafe(ggobj = chartzzz,
+               bg = "transparent")
+      })
+
+      output$year_breakdown <- renderReactable({
+        options(reactable.theme = reactableTheme(
+          style = list(
+            fontFamily = "Segoe UI"
+          )
+        ))
+
+        reactable(
+          bind_exp_inc %>%
+            mutate(date_year = year(format.POSIXct(date)),
+                   date = format.POSIXct(date)),
+          groupBy = c("date_year", "type", "category"),
+          columns = list(
+            date = colDef(name = "Date"),
+            date_year = colDef(name = "Year"),
+            type = colDef(name = "Type"),
+            category = colDef(name = "Category"),
+            amount = colDef(
+              name = "Amount",
+              aggregate = "sum",
+              format = colFormat(
+                currency = "USD",
+                separators = TRUE
+              )
+            )
+          )
+        )
       })
     })
   })
